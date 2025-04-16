98f952bc8f7c37bb3d8f23678abefcf24ca04ce0
Scarlett Parker <95775118+scarlettmparker@users.noreply.github.com>
2025-04-16 21:35:02 +0100

    Add lucide loader circle for loading texts

---
+++ client/src/components/lucide/loader-circle.tsx
@@ -0,0 +1,19 @@
+const LoaderCircle = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" style={{animation: "spin 1s linear infinite",}}>
+    <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    <style>
+      {`
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}
+    </style>
+  </svg>
+)
+
+export default LoaderCircle;
--- client/src/components/state/loading-state.tsx
+++ client/src/components/state/loading-state.tsx
@@ -1,15 +1,19 @@
 import { Component, JSX } from "solid-js";
+import { LoaderCircle } from "~/components/lucide";
+
+import styles from "./styles/loading-state.module.css";
 
 interface LoadingStateProps {
+  class?: string;
   children: JSX.Element | string;
 };
 
 const LoadingState: Component<LoadingStateProps> = (props) => {
-  const { children } = props;
+  const { class: class_ = "", children } = props;
 
   return (
-    <span>
-      {children}
+    <span class={`${styles.loading_state} ${class_}`}>
+      <LoaderCircle /> {children}
     </span>
   );
 };
