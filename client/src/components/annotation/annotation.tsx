6d441fec4278544d978161aba24d9f36f885294c
Scarlett Parker <95775118+scarlettmparker@users.noreply.github.com>
2025-04-20 00:49:37 +0100

    Render markdown in annotations

--- client/src/components/annotation/annotation.tsx
+++ client/src/components/annotation/annotation.tsx
@@ -1,9 +1,10 @@
-import { Component } from "solid-js";
+import { Component, createEffect, createSignal, onMount } from "solid-js";
 import { Card, CardBody, CardFooter, CardHeader } from "~/components/card";
 import LikeButton from "~/components/like-button";
 import ProfilePicture from "~/components/profile-picture";
 import { unixToDate } from "~/utils/annotation";
 import { AnnotationResponse } from "~/types";
+import { marked } from "marked";
 import styles from "./annotation.module.css";
 
 interface AnnotationProps {
@@ -13,10 +14,31 @@ interface AnnotationProps {
 const Annotation: Component<AnnotationProps> = (props) => {
   const { annotation } = props;
   const author = annotation.author;
+  const [markedDescription, setMarkedDescription] = createSignal<string>("");
+
+  onMount(() => {
+    const renderer = new marked.Renderer();
+
+    // Add target="_blank" to all links to open in new tab
+    renderer.link = function ({ href, title, text }) {
+      const target = "_blank";
+      const rel = "noopener noreferrer";
+      return `<a href="${href}" title="${title || ""}" target="${target}" rel="${rel}">${text}</a>`;
+    };
+
+    marked.setOptions({
+      breaks: true,
+      renderer: renderer,
+    });
+  });
+
+  createEffect(async () => {
+    setMarkedDescription(await marked(annotation.description));
+  });
 
   return (
     <Card class={styles.annotation}>
-      <a href={`/user/${author.discord_id}`}>
+      <a href={`/user/${author.discord_id}`} class={styles.annotation_link}>
         <CardHeader class={styles.annotation_header}>
           <ProfilePicture
             avatar={author.avatar}
@@ -29,7 +51,12 @@ const Annotation: Component<AnnotationProps> = (props) => {
           </span>
         </CardHeader>
       </a>
-      <CardBody>{annotation.description}</CardBody>
+      <CardBody>
+        <div
+          class={styles.annotation_description}
+          innerHTML={markedDescription()}
+        />
+      </CardBody>
       <CardFooter class={styles.annotation_footer}>
         <LikeButton class={styles.like_button} />
         <span class={styles.likes_count}>
