c2759702281fe6fc891409fbdb079ad163f79905
Scarlett Parker <95775118+scarlettmparker@users.noreply.github.com>
2025-04-24 23:33:10 +0100

    Minor login button styling and navigate to /login

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -1,11 +1,22 @@
 import { Card, CardHeader } from "~/components/card";
+import { useNavigate } from "react-router-dom";
 import styles from "./navbar.module.css";
 
 const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
     <Card className={styles.navbar}>
       <CardHeader className={styles.navbar_header}>
-        Login
+        <span
+          className={styles.navbar_login}
+          onClick={() => {
+            console.log("hi");
+            navigate("/login");
+          }}
+        >
+          Login
+        </span>
       </CardHeader>
     </Card>
   );
